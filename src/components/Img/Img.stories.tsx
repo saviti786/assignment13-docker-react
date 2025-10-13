3e170974022bf7fd12ce9cbefbfa0cf28257df5e
saviti786 <saviti@Academic.RRC.CA>
2025-10-12 19:48:39 -0500

    Img testing going on

---
+++ src/components/Img/Img.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/react-webpack5";
+import { Img } from "./Img";
+
+const meta: Meta<typeof Img> = {
+  title: "Components/Img",
+  component: Img,
+  tags: ["autodocs"],
+  argTypes: {
+    src: { control: "text" },
+    alt: { control: "text" },
+    width: { control: "text" },
+    height: { control: "text" },
+    disabled: { control: "boolean" },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Img>;
+
+export const Default: Story = {
+  args: {
+    src: "/happy.jpg",
+    alt: "Stay Happy",
+    width: "200px",
+    height: "200px",
+    disabled: false,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    src: "/happy.jpg",
+    alt: "Stay Happy",
+    width: "200px",
+    height: "200px",
+    disabled: true,
+  },
+};
